1fd55390e08f1e0064ed23b67260b9a9653e23d7
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-12-19 17:22:48 +0530

    fix: cmd-j ui changes

--- packages/docsearch-react/src/Footer.tsx
+++ packages/docsearch-react/src/Footer.tsx
@@ -22,19 +22,54 @@ const renderCmdOrCtrlKey = () => {
 };
 
 export function Footer() {
+  function setActiveCmdView(cmdKey: 'K' | 'L') {
+    document.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        key: cmdKey,
+        ctrlKey: !isMac() || undefined,
+        metaKey: isMac() || undefined,
+        bubbles: true, // Ensure the event bubbles.
+        cancelable: true, // Allow the event to be cancellable.
+      })
+    );
+  }
+
   return (
     <div className="doc-container">
       <div className="doc-footer-item doc-shortcut-active">
         <svg
           className="doc-icon"
-          viewBox="0 0 24 24"
-          width="1.2em"
-          height="1.2em"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
         >
           <path
+            d="M7.5 14.6667L3.99999 14.6667C3.64637 14.6667 3.30723 14.5262 3.05718 14.2762C2.80713 14.0261 2.66666 13.687 2.66666 13.3334V2.66671C2.66666 2.31309 2.80713 1.97395 3.05718 1.7239C3.30723 1.47385 3.64637 1.33337 3.99999 1.33337H8.66666L13.3333 6.00004V8.5"
+            stroke="#3366ff"
+            stroke-width="1.33333"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M8.66666 1.33337V6.00004H13.3333"
+            stroke="#3366ff"
+            stroke-width="1.33333"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.5 14C11.8807 14 13 12.8807 13 11.5C13 10.1193 11.8807 9 10.5 9C9.11929 9 8 10.1193 8 11.5C8 12.8807 9.11929 14 10.5 14Z"
+            stroke="#3366ff"
+            stroke-width="1.33333"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M13.5286 15.4714C13.7889 15.7318 14.2111 15.7318 14.4714 15.4714C14.7318 15.2111 14.7318 14.7889 14.4714 14.5286L13.5286 15.4714ZM13.4714 13.5286L13 13.0572L12.0572 14L12.5286 14.4714L13.4714 13.5286ZM14.4714 14.5286L13.4714 13.5286L12.5286 14.4714L13.5286 15.4714L14.4714 14.5286Z"
             fill="#3366ff"
-            d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6m4 18H6V4h7v5h5v11Z"
-          ></path>
+          />
         </svg>
         Document
         <span className="doc-shortcut doc-icon-active">
@@ -44,19 +79,31 @@ export function Footer() {
       <div className="doc-footer-item">
         <svg
           className="doc-icon"
-          viewBox="0 0 24 24"
-          width="1.2em"
-          height="1.2em"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
         >
           <path
-            fill="#65696f"
-            d="M12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5M12 2a7 7 0 0 1 7 7c0 5.25-7 13-7 13S5 14.25 5 9a7 7 0 0 1 7-7m0 2a5 5 0 0 0-5 5c0 1 0 3 5 9.71C17 12 17 10 17 9a5 5 0 0 0-5-5Z"
-          ></path>
+            d="M10 9.33335L13.3333 6.00002L10 2.66669"
+            stroke="#65696f"
+            stroke-width="1.33333"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M2.66669 13.3333V8.66667C2.66669 7.95942 2.94764 7.28115 3.44774 6.78105C3.94783 6.28095 4.62611 6 5.33335 6H13.3334"
+            stroke="#65696f"
+            stroke-width="1.33333"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
         </svg>
         Quick Navigation
         <span className="doc-shortcut">{renderCmdOrCtrlKey()} + K</span>
       </div>
-      <div className="doc-footer-item">
+      <div className="doc-footer-item" onClick={() => setActiveCmdView('L')}>
         <svg
           className="doc-icon"
           viewBox="0 0 24 24"
